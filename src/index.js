7a73d9063c0d4bc076e61b2c1532b374e260d1b6
ZumTheZazaKing <zahidi85543@gmail.com>
2021-07-25 16:22:51 +0800

    The beginning

--- src/index.js
+++ src/index.js
@@ -2,11 +2,64 @@ import React from 'react';
 import { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-function Calculator(){
+
+function InputField(props){
+
+  return <input type="text" value={props.data} />
+
+}
+
+function Button(props){
 
   return <div>
-    <h1>Hello</h1>
+
+      <button onClick={}>C</button>
+      <button onClick={}>/</button>
+      <button onClick={}>*</button>
+      <button onClick={}>7</button>
+      <button onClick={}>8</button>
+      <button onClick={}>9</button>
+      <button onClick={}>-</button>
+      <button onClick={}>4</button>
+      <button onClick={}>5</button>
+      <button onClick={}>6</button>
+      <button onClick={}>+</button>
+      <button onClick={}>3</button>
+      <button onClick={}>2</button>
+      <button onClick={}>1</button>
+      <button onClick={}>0</button>
+      <button onClick={}>00</button>
+      <button onClick={}>.</button>
+      <button onClick={}>=</button>
+
+      
+
+    </div>
+
+}
+
+
+
+function Calculator(){
+
+  const [value, setValue] = useState("");
+
+  function changeValue(addition){
+
+    //setValue([...value, addition])
+
+    console.log(addition)
+
+  }
+
+
+  return <div>
+
+    <InputField data={value} />
+    <Button handleClick={changeValue} />
+
   </div>
+
 
 }
 
